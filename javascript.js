2da65aab1011a426193063a789cedfc6fc39dead
fiii <shofinur72@gmail.com>
2025-02-15 19:45:57 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,18 +8,12 @@ function moveButton() {
     button.style.top = `${y}px`; // Atur posisi Y secara acak
 }
 function nextPage() {
-    let name = prompt("Masukkan nama Anda:"); // Meminta input dari pengguna
-    if (name) {
-        fetch("https://script.google.com/macros/s/AKfycbyXEGnuM2GvSw4xwSBMgjPLS3kHthgCPtRlCDH3H35LkSEG7seb0vTzbMurDsylqid5vQ/exec" + encodeURIComponent(name))
-            .then(response => response.text()) // Mengubah response ke teks
-            .then(data => {
-                console.log("Response dari server:", data);
-                alert("Data berhasil dikirim!"); // Notifikasi ke pengguna
-                
-        window.location.href = "yes.html"; // Arahkan ke halaman lain setelah klik "Yes"
-            })
-            .catch(error => console.error("Gagal mengirim data:", error));
-    }
+    let name = prompt("Masukkan nama Anda:");
+    fetch("https://script.google.com/macros/s/AKfycbyXEGnuM2GvSw4xwSBMgjPLS3kHthgCPtRlCDH3H35LkSEG7seb0vTzbMurDsylqid5vQ/exec" + encodeURIComponent(name))
+        .then(response => console.log("Data terkirim!"))
+        .catch(error => console.error("Gagal mengirim data:", error));
+
+    window.location.href = "yes.html"; // Gantilah dengan halaman yang Anda inginkan
 }
 function doGet(e) {
   var sheet = SpreadsheetApp.openById("14i2900Sias7Nr-N_RfOIuoii9iMJ9M5OhGUzw2OzckQ").getSheetByName("Sheet1");
